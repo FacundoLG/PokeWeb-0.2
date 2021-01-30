83b1cd68e251d56f9aa303278b6776a958fea131
FacundoLG <facu-judoca@hotmail.com.ar>
2021-01-29 23:01:56 -0300

    Agregados los elementos y algunos pokemones

--- index/index.js
+++ index/index.js
@@ -1,7 +1,15 @@
 const data = document.getElementById("dataContainer")
-pokeLeght = 12
+pokeLeght = 13
 for(i = 0;i < pokeLeght;i++){
     var card = document.createElement("div")
         card.setAttribute("class","base");
+        card.setAttribute("id",`number${i}`)
         data.appendChild(card)
+    var base = document.getElementById(`number${i}`)    
+    var baseImage = document.createElement("div")
+        baseImage.setAttribute("class","base-image")
+        base.appendChild(baseImage)
+    var dato = document.createElement("div")
+        dato.setAttribute("class","pokeData")
+        base.appendChild(dato)
 }
